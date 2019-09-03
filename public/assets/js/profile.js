efb83890d26c461a7796f02989372bdd75cc6c5b
Yousuf Khalid <codinghackers@gmail.com>
2019-09-03 17:33:56 +0500

    neighborhoods experties and language input fields

---
+++ public/assets/js/profile.js
@@ -0,0 +1,8 @@
+$(() => {
+    $('body').on('keydown', '.amsify-suggestags-input', function(event){
+        if(event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+});
