8869181c13fb05ae4244936c9192e2e26ade6a1c
Yousuf Khalid <codinghackers@gmail.com>
2019-09-09 16:07:05 +0500

    bug resolved

--- resources/views/agent/listing-features/listing_info.blade.php
+++ resources/views/agent/listing-features/listing_info.blade.php
@@ -118,7 +118,7 @@
                     @if(isset($listing->thumbnail))
                         {!! Form::hidden('old_thumbnail', $listing->thumbnail) !!}
                     @endif
-					<img class="img-thumbnail" src="{{($edit && isset($listing->thumbnail)) ? asset('storage/'.$listing->thumbnail) : ''}}" id="img" style="{{($edit) ? 'width: 180px;height: 145px;margin-bottom: 15px;' : ''}}">
+					<img class="img-thumbnail" src="{{($action && isset($listing->thumbnail)) ? asset('storage/'.$listing->thumbnail) : ''}}" id="img" style="{{($action) ? 'width: 180px;height: 145px;margin-bottom: 15px;' : ''}}">
 				</div>
                 <div class="col-md-6"></div>
 				<div class="col-md-12">
