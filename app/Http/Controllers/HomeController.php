6479bd8e326027796a9624a2f9d26c14b302a2f2
HBilalSaqib <bilal.saqib@techverx.com>
2019-09-18 16:14:09 +0500

    -Newsletter for wordpress.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends Controller {
 	 * @return \Illuminate\Contracts\Support\Renderable
 	 */
 	public function index() {
-		$featured_listings = $this->service->featured_listing($this->paginate); 
+		$featured_listings = $this->service->featured_listing($this->paginate);
 		return view('index', compact('featured_listings'));
 	}
 
