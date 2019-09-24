a4f3f2557078a933f21eb95200823dececc982ef
Yousuf Khalid <codinghackers@gmail.com>
2019-09-24 17:07:53 +0500

    feature listing

--- resources/views/features/feature_listing.blade.php
+++ resources/views/features/feature_listing.blade.php
@@ -17,34 +17,6 @@
             <div class="tab-pane active" id="tab1">
                 <div class="property-listing">
                     @foreach($featured_listings["recent"] as $key => $fl)
-                    <div class="property-thumb">
-                        <div class="check-btn">
-                            <a href="{{ route('listing.detail', $fl->id) }}">
-                                <button class="btn-default" data-toggle="modal" data-target="#check-availability">Check Availability</button>
-                            </a>
-                        </div>
-                        <span class="heart-icon"></span>
-                        <img src="{{ asset('storage/'.$fl->thumbnail) }}" alt="" class="main-img" />
-                        <div class="info">
-                            <a href="#" class="info-link-text">
-                                <p> $ {{ $fl->rent }} </p> <small> {{ $fl->bedrooms }} {{ $fl->bedrooms > 1 ? 'beds' : 'bed' }} , {{ $fl->baths }} {{ $fl->baths > 1 ? 'baths' : 'bath' }} </small>
-                                <p> {{ $fl->display_address }} </p>
-                            </a>
-                            <a href="javascript:void(0)" class="btn viewfeature-btn"> View </a>
-                        </div>
-                        <div class="feaure-policy-text">
-                            <p>${{ $fl->rent }} / Month </p>
-                            <span>2 bed , 1 bath </span>
-                        </div>
-
-                    </div>
-                    @endforeach
-                </div>
-            </div>
-            <div class="tab-pane" id="tab2">
-                <div class="property-listing">
-                    <div class="desktop-listiing">
-                        @foreach($featured_listings["recent"] as $key => $fl)
                         <div class="property-thumb">
                             <div class="check-btn">
                                 <a href="{{ route('listing.detail', $fl->id) }}">
@@ -66,63 +38,91 @@
                             </div>
 
                         </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="tab-pane" id="tab2">
+                <div class="property-listing">
+                    <div class="desktop-listiing">
+                        @foreach($featured_listings["recent"] as $key => $fl)
+                            <div class="property-thumb">
+                                <div class="check-btn">
+                                    <a href="{{ route('listing.detail', $fl->id) }}">
+                                        <button class="btn-default" data-toggle="modal" data-target="#check-availability">Check Availability</button>
+                                    </a>
+                                </div>
+                                <span class="heart-icon"></span>
+                                <img src="{{ asset('storage/'.$fl->thumbnail) }}" alt="" class="main-img" />
+                                <div class="info">
+                                    <a href="#" class="info-link-text">
+                                        <p> $ {{ $fl->rent }} </p> <small> {{ $fl->bedrooms }} {{ $fl->bedrooms > 1 ? 'beds' : 'bed' }} , {{ $fl->baths }} {{ $fl->baths > 1 ? 'baths' : 'bath' }} </small>
+                                        <p> {{ $fl->display_address }} </p>
+                                    </a>
+                                    <a href="javascript:void(0)" class="btn viewfeature-btn"> View </a>
+                                </div>
+                                <div class="feaure-policy-text">
+                                    <p>${{ $fl->rent }} / Month </p>
+                                    <span>2 bed , 1 bath </span>
+                                </div>
+
+                            </div>
                         @endforeach
                     </div>
 
                     <div class="owl-slider">
                         <div class="owl-carousel owl-theme" id="carousel-1">
-                                @foreach($featured_listings["cheapest"] as $key => $fl)
-                            <div class="item">
-                                <div class="property-thumb">
-                                    <div class="check-btn">
-                                        <a href="{{ route('listing.detail', $fl->id) }}">
-                                            <button class="btn-default" data-toggle="modal" data-target="#check-availability">Check Availability</button>
-                                        </a>
-                                    </div>
-                                    <span class="heart-icon"></span>
-                                    <img src="{{ asset('storage/'.$fl->thumbnail) }}" alt="" class="main-img" />
-                                    <div class="info">
-                                        <a href="#">
-                                            <p>$ {{ $fl->rent }} </p>
-                                            <ul>
-                                                <li><i class="fa fa-bed"></i> {{ $fl->bedrooms }}</li>
-                                                <li><i class="fa fa-bath"></i> {{ $fl->baths }}</li>
-                                            </ul>
-                                        </a>
-                                        <a href="javascript:void(0)" class="btn viewfeature-btn"> View </a>
+                            @foreach($featured_listings["cheapest"] as $key => $fl)
+                                <div class="item">
+                                    <div class="property-thumb">
+                                        <div class="check-btn">
+                                            <a href="{{ route('listing.detail', $fl->id) }}">
+                                                <button class="btn-default" data-toggle="modal" data-target="#check-availability">Check Availability</button>
+                                            </a>
+                                        </div>
+                                        <span class="heart-icon"></span>
+                                        <img src="{{ asset('storage/'.$fl->thumbnail) }}" alt="" class="main-img" />
+                                        <div class="info">
+                                            <a href="#">
+                                                <p>$ {{ $fl->rent }} </p>
+                                                <ul>
+                                                    <li><i class="fa fa-bed"></i> {{ $fl->bedrooms }}</li>
+                                                    <li><i class="fa fa-bath"></i> {{ $fl->baths }}</li>
+                                                </ul>
+                                            </a>
+                                            <a href="javascript:void(0)" class="btn viewfeature-btn"> View </a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                                @endforeach
+                            @endforeach
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="tab-pane" id="tab4">
+            <div class="tab-pane" id="tab4">
             <div class="property-listing">
                 @foreach($featured_listings["pet_policy"] as $key => $fl)
-                <div class="property-thumb">
-                    <div class="check-btn">
-                        <a href="{{ route('listing.detail', $fl->id) }}">
-                            <button class="btn-default" data-toggle="modal" data-target="#check-availability">Check Availability</button>
-                        </a>
+                    <div class="property-thumb">
+                        <div class="check-btn">
+                            <a href="{{ route('listing.detail', $fl->id) }}">
+                                <button class="btn-default" data-toggle="modal" data-target="#check-availability">Check Availability</button>
+                            </a>
+                        </div>
+                        <span class="heart-icon"></span>
+                        <img src="{{ asset('storage/'.$fl->thumbnail) }}" alt="" class="main-img" />
+                        <div class="info">
+                            <a href="#">
+                                <p>$ {{ $fl->rent }} </p>
+                                <ul>
+                                    <li><i class="fa fa-bed"></i> {{ $fl->bedrooms }}</li>
+                                    <li><i class="fa fa-bath"></i> {{ $fl->baths }}</li>
+                                </ul>
+                            </a>
+                            <a href="javascript:void(0)" class="btn viewfeature-btn"> View </a>
+                        </div>
                     </div>
-                    <span class="heart-icon"></span>
-                    <img src="{{ asset('storage/'.$fl->thumbnail) }}" alt="" class="main-img" />
-                    <div class="info">
-                        <a href="#">
-                            <p>$ {{ $fl->rent }} </p>
-                            <ul>
-                                <li><i class="fa fa-bed"></i> {{ $fl->bedrooms }}</li>
-                                <li><i class="fa fa-bath"></i> {{ $fl->baths }}</li>
-                            </ul>
-                        </a>
-                        <a href="javascript:void(0)" class="btn viewfeature-btn"> View </a>
-                    </div>
-                </div>
                 @endforeach
             </div>
+        </div>
         </div>
         <div class="text-center">
             {{--<a href="#" class="btn-default">view all</a>--}}
@@ -146,7 +146,7 @@
                         <div class="col-lg-8 padding-leftt-0">
                             <h3> 345 SOUTH END AVENUE, #5P</h3>
                             <strong> $2,815 </strong>
-                            <!--  <small>For Rental</small> -->
+                            <!-- <small>For Rental</small> -->
                             <div class="after-border"></div>
                             <div class="bedroms-baths-text">
                                 <i class="fas fa-home"></i> <span> 2 Bedrooms, 3 Rooms, 2 Baths</span>
