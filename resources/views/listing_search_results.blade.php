e2f655c110795b72aa55284bd18ee47850c91fc5
muhammad-irfan123 <m.irfan@techverx.com>
2019-10-07 11:13:17 +0500

    -"Profile page updated"
    Signed-off-by: muhammad-irfan123 <m.irfan@techverx.com>

--- resources/views/listing_search_results.blade.php
+++ resources/views/listing_search_results.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <section class="neighborhood-search neighbourhood-pd wow fadeIn" data-wow-delay="0.2s">
         {{--Search Results--}}
-        @include('sections.search_results')
+        <div class="container-lg">
+        	@include('sections.search_results')
+    	</div>
     </section>
 @endsection
