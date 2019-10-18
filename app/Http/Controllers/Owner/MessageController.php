e53f5c12461824b379506be125a2c6fbf76c37da
HBilalSaqib <bilal.saqib@techverx.com>
2019-10-18 15:24:20 +0500

    -Owner messaging module added.

--- app/Http/Controllers/Owner/MessageController.php
+++ app/Http/Controllers/Owner/MessageController.php
@@ -2,8 +2,138 @@
 
 namespace App\Http\Controllers\Owner;
 
-use App\Services\ListingConversationService;
+/*use App\Services\ListingConversationService;
 use App\Services\CheckAvailabilityService;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;*/
+
+/**
+ * Class MessageController
+ * @package App\Http\Controllers\Owner
+ */
+/*class MessageController extends Controller {*/
+
+    /**
+     * @var int
+     */
+/*    private $paginate = 20;*/
+
+    /**
+     * @var ListingConversationService
+     */
+/*    private $appointmentService;*/
+
+    /**
+     * @var CheckAvailabilityService
+     */
+/*    private $checkAvailabilityService;*/
+
+    /**
+     * MessageController constructor.
+     */
+/*    public function __construct() {
+        $this->appointmentService = new ListingConversationService();
+        $this->checkAvailabilityService = new CheckAvailabilityService();
+    }*/
+
+    /**
+     * @return Factory|View
+     */
+/*    public function index() {
+        $data = toObject([
+            'appointments'   => $this->appointmentService->fetchAppointments($this->paginate),
+            'availabilities' => $this->checkAvailabilityService->fetchAvailabilities($this->paginate)
+        ]);
+        return view('agent.message', compact('data'));
+    }*/
+
+    /**
+     * @param Request $request
+     * @param $id
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+/*    public function accept(Request $request, $id) {
+        $data = null;
+        if($this->appointmentService->accept($id))
+            $data = $this->appointmentService->messages($id);
+        return sendResponse($request, $data);
+    }*/
+
+    /**
+     * @param Request $request
+     * @param $id
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+/*    public function archiveInbox(Request $request, $id) {
+        $response = $this->appointmentService->archive($id);
+        return sendResponse($request, $response, 'Chat added to Archive.');
+    }*/
+
+    /**
+     * @param Request $request
+     * @param $id
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+/*    public function archiveAvailability(Request $request, $id) {
+        $response = $this->checkAvailabilityService->archive($id);
+        return sendResponse($request, $response, 'Chat added to Archive.');
+    }*/
+
+    /**
+     * @param $id
+     *
+     * @return Factory|View
+     */
+/*    public function loadInbox($id) {
+        $collection = toObject($this->appointmentService->messages($id));
+        return view('agent.inbox', compact('collection'))->with('route', 'agent.sendInboxMessage');
+    }*/
+
+    /**
+     * @param $id
+     *
+     * @return Factory|View
+     */
+/*    public function loadAvailability($id) {
+        $collection = toObject($this->checkAvailabilityService->messages($id));
+        return view('agent.inbox', compact('collection'))->with('route', 'agent.sendAvailabilityMessage');
+    }*/
+
+    /**
+     * @param Request $request
+     * @param $id
+     *
+     * @return JsonResponse
+     */
+/*    public function replyInbox(Request $request, $id) {
+        $res = $this->appointmentService->reply($id, $request);
+        return sendResponse($request, $res, null);
+    }*/
+
+    /**
+     * @param Request $request
+     * @param $id
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+/*    public function replyAvailability(Request $request, $id) {
+        $res = $this->checkAvailabilityService->reply($id, $request);
+        return sendResponse($request, $res, null);
+    }
+}*/
+
+// UPDATED CODE
+
+
+
+use App\Services\ListingConversationService;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
@@ -12,8 +142,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\View\View;
 
 /**
- * Class MessageController
- * @package App\Http\Controllers\Owner
+ * Class ListingConversationController
+ * @package App\Http\Controllers\Agent
  */
 class MessageController extends Controller {
 
@@ -25,30 +155,21 @@ class MessageController extends Controller {
     /**
      * @var ListingConversationService
      */
-    private $appointmentService;
-
-    /**
-     * @var CheckAvailabilityService
-     */
-    private $checkAvailabilityService;
+    private $conversationService;
 
     /**
      * MessageController constructor.
      */
     public function __construct() {
-        $this->appointmentService = new ListingConversationService();
-        $this->checkAvailabilityService = new CheckAvailabilityService();
+        $this->conversationService = new ListingConversationService();
     }
 
     /**
      * @return Factory|View
      */
     public function index() {
-        $data = toObject([
-            'appointments'   => $this->appointmentService->fetchAppointments($this->paginate),
-            'availabilities' => $this->checkAvailabilityService->fetchAvailabilities($this->paginate)
-        ]);
-        return view('agent.message', compact('data'));
+        $conversations = toObject($this->conversationService->fetchConversations($this->paginate));
+        return view('owner.message', compact('conversations'));
     }
 
     /**
@@ -59,20 +180,13 @@ class MessageController extends Controller {
      */
     public function accept(Request $request, $id) {
         $data = null;
-        if($this->appointmentService->accept($id))
-            $data = $this->appointmentService->messages($id);
+        if($this->conversationService->accept($id))
+            $data = $this->conversationService->loadMessages($id);
         return sendResponse($request, $data);
     }
 
-    /**
-     * @param Request $request
-     * @param $id
-     *
-     * @return JsonResponse|RedirectResponse
-     */
-    public function archiveInbox(Request $request, $id) {
-        $response = $this->appointmentService->archive($id);
-        return sendResponse($request, $response, 'Chat added to Archive.');
+    public function deny() {
+
     }
 
     /**
@@ -81,9 +195,20 @@ class MessageController extends Controller {
      *
      * @return JsonResponse|RedirectResponse
      */
-    public function archiveAvailability(Request $request, $id) {
-        $response = $this->checkAvailabilityService->archive($id);
-        return sendResponse($request, $response, 'Chat added to Archive.');
+    public function archive(Request $request, $id) {
+        $response = $this->conversationService->archive($id);
+        return sendResponse($request, $response, 'Conversation Added to Archive.');
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+    public function unArchive(Request $request, $id) {
+        $response = $this->conversationService->unArchive($id);
+        return sendResponse($request, $response, 'Conversation Removed from Archive.');
     }
 
     /**
@@ -91,19 +216,9 @@ class MessageController extends Controller {
      *
      * @return Factory|View
      */
-    public function loadInbox($id) {
-        $collection = toObject($this->appointmentService->messages($id));
-        return view('agent.inbox', compact('collection'))->with('route', 'agent.sendInboxMessage');
-    }
-
-    /**
-     * @param $id
-     *
-     * @return Factory|View
-     */
-    public function loadAvailability($id) {
-        $collection = toObject($this->checkAvailabilityService->messages($id));
-        return view('agent.inbox', compact('collection'))->with('route', 'agent.sendAvailabilityMessage');
+    public function load($id) {
+        $collection = $this->conversationService->loadMessages($id);
+        return view('agent.inbox', compact('collection'));
     }
 
     /**
@@ -112,19 +227,8 @@ class MessageController extends Controller {
      *
      * @return JsonResponse
      */
-    public function replyInbox(Request $request, $id) {
-        $res = $this->appointmentService->reply($id, $request);
-        return sendResponse($request, $res, null);
-    }
-
-    /**
-     * @param Request $request
-     * @param $id
-     *
-     * @return JsonResponse|RedirectResponse
-     */
-    public function replyAvailability(Request $request, $id) {
-        $res = $this->checkAvailabilityService->reply($id, $request);
+    public function reply(Request $request, $id) {
+        $res = $this->conversationService->reply($id, $request);
         return sendResponse($request, $res, null);
     }
 }
--- resources/views/owner/message.blade.php
+++ resources/views/owner/message.blade.php
@@ -10,22 +10,17 @@
                 <ul class="nav nav-pills">
                     <li class="nav-item">
                         <a class="nav-link active" data-toggle="pill" href="#tab-1">
-                            Inbox ({{--{{ $data->appointments->active->total() }}--}})
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="pill" href="#tab-4">
-                            Checking Availability ({{--{{ $data->availabilities->total() }}--}})
+                            Inbox ({{ $conversations->active->total() }})
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" data-toggle="pill" href="#tab-2">
-                            Archived
+                            Archived ({{ $conversations->archived->total() }})
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" data-toggle="pill" href="#tab-3">
-                            Meeting Request ({{--{{ $data->appointments->inactive->total() }}--}})
+                            Meeting Request ({{ $conversations->inactive->total() }})
                         </a>
                     </li>
                 </ul>
@@ -35,41 +30,22 @@
                 <div class="tab-content">
                     {{--Inbox--}}
                     <div class="tab-pane active" id="tab-1">
-                        @include('agent.message-tabs.inbox')
+                        @include('owner.message-tabs.inbox')
                     </div>
                     {{--Archive--}}
                     <div class="tab-pane fade" id="tab-2">
-                        <div class="message-row">
-                            <h3>Elitan</h3>
-                            <p>Reminder from nofeerentals: You have still not replied to Ethan in regards Reminder from RentHop: You have still not replied to... <a href="" data-toggle="modal" data-target="#message-modal">Read More</a></p>
-                            <div class="property">
-                                <img src="assets/images/listing-img.jpg" alt="" />
-                                <div class="info">
-                                    <ul>
-                                        <li><i class="fa fa-bed"></i> 2 Bed</li>
-                                        <li><i class="fa fa-bath"></i> 1 Bath</li>
-                                    </ul>
-                                    <p>at West 96th Street,</p>
-                                    <div class="price"><i class="fa fa-tag"></i> $5,870</div>
-                                </div>
-                            </div>
-                            <div class="date-text"> Sun, 05/5/19, 10:14 am</div>
-                        </div>
+                        @include('owner.message-tabs.archived')
                     </div>
                     {{--Request Meeting--}}
                     <div class="tab-pane fade" id="tab-3">
-                        {{--@include('agent.message-tabs.meeting_request')--}}
-                    </div>
-                    {{--Availability--}}
-                    <div class="tab-pane fade" id="tab-4">
-                        {{--@include('agent.message-tabs.availabilities')--}}
+                        @include('owner.message-tabs.meeting_request')
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    {{--Agent Messaging Modal--}}
-    {{--@include('agent.modals.message')--}}
+    {{--Owner Messaging Modal--}}
+    @include('owner.modals.message')
 
     {{--Messaging Script--}}
     {!! HTML::script('assets/js/message.js') !!}
