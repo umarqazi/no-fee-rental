a4d0987650e5ddf050485296d23cfe11d48b70c8
Yousuf Khalid <yousuf.khalid@techverx.com>
2019-10-30 13:31:55 +0500

    renter profile

--- app/Http/Controllers/Renter/ProfileController.php
+++ app/Http/Controllers/Renter/ProfileController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers\Renter;
 
+use App\Services\UserService;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\View\View;
 
 class ProfileController extends Controller {
-    //
+
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    /**
+     * ProfileController constructor.
+     *
+     * @param UserService $service
+     */
+    public function __construct(UserService $service) {
+        $this->userService = $service;
+    }
+
+    /**
+     * @return Factory|View
+     */
+    public function profile() {
+        $user = mySelf();
+        $exclusiveSettings = $this->userService->getExclusiveSettings(myId());
+        return view('renter.profile', compact('user', 'exclusiveSettings'));
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function updateProfile(Request $request) {
+        $res = $this->userService->updateProfile($request);
+        return sendResponse($request, $res, 'Profile has been updated.');
+    }
 }
