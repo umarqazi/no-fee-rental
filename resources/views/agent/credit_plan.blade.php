5bb8f57baa7e13976c193b81bc0eddd312af8653
muhammad-irfan123 <m.irfan@techverx.com>
2019-11-05 19:01:39 +0500

    --"checkout popup"

--- resources/views/agent/credit_plan.blade.php
+++ resources/views/agent/credit_plan.blade.php
@@ -1,6 +1,9 @@
 @extends('secured-layouts.app')
 @section('title', 'Credit Plan')
 @section('content')
+    <link rel="stylesheet" type="text/css" href="http://no-fee-rental.teamtechverx.com/assets/css/datepicker.min.css">
+    <script src="http://no-fee-rental.teamtechverx.com/assets/js/datepicker.min.js"></script>
+    <script src="http://no-fee-rental.teamtechverx.com/assets/js/datepicker.en.js"></script>
     <div class="wrapper">
         <div class="heading-wrapper">
             <h1>Credits Plan</h1>
@@ -23,7 +26,7 @@
                                 <li>20 reposts monthly</li>
                             </ul>
                         </div>
-                        <a href="{{route('agent.basicPlan')  }}" class="btn btn-default"> Current plan </a>
+                        <a href="{{route('agent.basicPlan')  }}" class="btn btn-default" data-toggle="modal" data-target="#myModal-currentPlan"> Current plan </a>
                     </div>
                     <div class="current-plans gold-plan">
                         <h3> Gold Plan</h3>
@@ -51,4 +54,55 @@
             </div>
         </div>
     </div>
+
+    {{--    checkout Modal--}}
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal-currentPlan">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title text-center">Purchase Basic Plan</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Cardholder Name</label>
+                        <input type="text" class="input-style" />
+                    </div>
+                    <div class="form-group">
+                        <label>Card Number</label>
+                        <input type="text" class="input-style" />
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Expiration date</label>
+                                <input autocomplete="off" id="open_checkout-calendar" class="input-style" name="opencheckout" type="text">
+                                <i class="far fa-calendar-alt"></i>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Cw</label>
+                                <input type="text" class="input-style" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default checkout-popup-btn" data-dismiss="modal">Checkout</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script>
+        enableDatePicker('#open_checkout-calendar', false);
+    </script>
 @endsection
