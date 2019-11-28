f58bb0f0a3a24f80dd5eca66275de053c9744cee
yousufkhalid223 <yousuf.khalid@techverx.com>
2019-11-28 16:03:52 +0500

    wp auth updated

--- app/Http/Controllers/WP/AuthController.php
+++ app/Http/Controllers/WP/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\WP;
 
+use App\Services\AuthService;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
@@ -12,10 +14,14 @@ use App\Http\Controllers\Controller;
  */
 class AuthController extends Controller {
 
+    use AuthenticatesUsers;
+
     /**
      * @var object
      */
     private $data;
+
+    private $guard;
 
     /**
      * @param Request $request
@@ -25,7 +31,7 @@ class AuthController extends Controller {
     public function login(Request $request) {
         $this->data = toObject($request->all());
         if($user = $this->__verifyUser()) {
-
+            $this->__webLogin($request);
             return response()->json([
                 'status' => true,
                 'guard' => $this->__guardAssign($user),
@@ -38,6 +44,35 @@ class AuthController extends Controller {
             'status' => false,
             'msg' => 'Wrong email or password',
         ], 401);
+    }
+
+    /**
+     * @param $request
+     * @return bool|\Symfony\Component\HttpFoundation\Response|void
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    private function __webLogin($request) {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return true;
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     /**
@@ -61,19 +96,28 @@ class AuthController extends Controller {
         $guard = null;
         switch($user->user_type) {
             case ADMIN:
-                $guard = 'admin';
+                $this->guard = 'admin';
                 break;
             case AGENT:
-                $guard = 'agent';
+                $this->guard = 'agent';
                 break;
             case OWNER:
-                $guard = 'owner';
+                $this->guard = 'owner';
                 break;
             case RENTER:
-                $guard = 'renter';
+                $this->guard = 'renter';
                 break;
         }
 
         return $guard;
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard(){
+        return Auth::guard($this->guard);
     }
 }
