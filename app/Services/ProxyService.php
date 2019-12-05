f3f077ecb261af59826442a2ede765386975decb
yousufkhalid223 <yousuf.khalid@techverx.com>
2019-12-05 13:43:15 +0500

    school zone updates

--- app/Services/ProxyService.php
+++ app/Services/ProxyService.php
@@ -76,17 +76,8 @@ class ProxyService {
 
         if(!empty($res)) {
             foreach ($res as $geom) {
-//                $coordinates = [];
-//                $geom = $geom['the_geom']['coordinates'][0][0];
-//                for($i = 0; $i < sizeof($geom); $i ++) {
-//                    $coords = [
-//                        'longitude' => (float)$geom[$i][0],
-//                        'latitude' => (float)$geom[$i][1]
-//                    ];
-//
-//                    array_push($coordinates, $coords);
-//                }
-dd($geom);
+//                $this->schoolData($)
+                dd($geom);
                 array_push($collection, $geom['the_geom']['coordinates'][0]);
             }
         }
