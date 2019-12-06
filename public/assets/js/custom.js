2db90a6ebc34ed3c1984bffee85398959d87869e
muhammad-irfan123 <m.irfan@techverx.com>
2019-12-06 19:10:27 +0500

    --"on load change banner images and create new listing form "

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,11 +1,14 @@
 $(() => {
     new WOW().init();
 
-
     $("header .menu-icon").click(function () {
         $(".main-wrapper aside").slideDown();
     });
 
+    //randomly change banner images on load
+    let image = ["home-banner-new.jpeg" , "banner-bg.jpg" , "large-view-9.jpg"];
+    let x = Math.floor(image.length * Math.random());
+    $('body').find('.header-bg').css({"background": "url(../assets/images/" + image[x] + ")"});
 
     $("aside .close-menu").click(function () {
         $(".main-wrapper aside").slideUp();
