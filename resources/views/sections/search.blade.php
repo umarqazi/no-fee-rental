c2ecb3c88376f07a329bc5540b0afb7fadaecb40
muhammad-irfan123 <m.irfan@techverx.com>
2019-12-17 21:11:06 +0500

    --"multiselect dropdown advance search."

--- resources/views/sections/search.blade.php
+++ resources/views/sections/search.blade.php
@@ -7,11 +7,39 @@
                 <i class="fas fa-search"></i>
                 {!! Form::text('neighborhoods', null, ['id' => 'neigh', 'placeholder' => 'Enter Neighborhood', 'class' => 'search-fld']) !!}
                 {!! Form::select('priceRange',  config('formfields.search.price') ,null, ['class' => 'custom-select-input','id' => 'main-search-priceRange']) !!}
-                {!! Form::select('beds', config('formfields.search.beds'),null, ['class' => 'custom-select-input size-input','id' => 'main-search-beds']) !!}
+                {{--                {!! Form::select('beds', config('formfields.search.beds'),null, ['class' => 'custom-select-input size-input','id' => 'main-search-beds']) !!}--}}
+
+            </div>
+            <div class="dropdown-beds" >
+                <label id="show-beds"><span class="placeholder-text">Beds</span></label>
+                <div id="advance-search-chkbox" class="beds-dropdown">
+                    <div id="advance-search-beds">
+                        <ul>
+                            <li> <input type="checkbox" value="studio" id="Checkbox-ad-search" name="Checkbox">
+                                <label for="Checkbox-ad-search"><span class="label-name">Studio</span></label>
+                            </li>
+                            <li> <input type="checkbox" value="1" id="Checkbox-1-ad-search" name="beds[]">
+                                <label for="Checkbox-1-ad-search"><span class="label-name">1</span></label>
+                            </li>
+                            <li> <input type="checkbox" value="2" id="Checkbox-2-ad-search" name="beds[]">
+                                <label for="Checkbox-2-ad-search"><span class="label-name">2</span></label>
+                            </li>
+                            <li> <input type="checkbox" value="3" id="Checkbox-3-ad-search" name="beds[]">
+                                <label for="Checkbox-3-ad-search"><span class="label-name">3</span></label>
+                            </li>
+                            <li> <input type="checkbox" value="4" id="Checkbox-4-ad-search" name="beds[]">
+                                <label for="Checkbox-4-ad-search"><span class="label-name">4</span></label>
+                            </li>
+                            <li> <input type="checkbox" value="5" id="Checkbox-5-ad-search" name="beds[]">
+                                <label for="Checkbox-5-ad-search"><span class="label-name">5+</span></label>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             {!! Form::button('Search', ['class' => 'search-btn', 'type' => 'submit']) !!}
-
         </div>
+
         <span id="search-error-message"></span>
         {!! Form::close() !!}
 
