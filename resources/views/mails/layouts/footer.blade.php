369316fc03109b27bd537786f72b0706e2b7dca6
yousufkhalid223 <yousuf.khalid@techverx.com>
2020-02-04 19:13:10 +0500

    mailchimp icon

--- resources/views/mails/layouts/footer.blade.php
+++ resources/views/mails/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="social-icons">
         <a href="javascript:void(0)"><img src="{{ asset('assets/images/twitter-icon.png') }}"> </a>
         <a href="javascript:void(0)"><img src="{{ asset('assets/images/fb-icon.png') }}"> </a>
-        <a href="javascript:void(0)"><img src="{{ asset('assets/images/mail-c.png') }}"> </a>
+        <a href="javascript:void(0)"><img src="{{ asset('assets/images/mailchimp.png') }}"> </a>
     </div>
     <p style="font-style: italic;">Copyright ©{{ now()->format('Y') }} <a href="#">NOFEERENTALSNYC.COM</a>, All rights reserved.</p>
     <p class="footer-adres-text"> Our mailing address is: <br> 447 BROADWAY <br> 2nd FL #453 <br> New York, NY 10013 </p>
